589509d5d2a3a52b3025c0bc869b021484013a7c
chuksremi15 <chuksremi15@gmail.com>
2024-09-02 23:29:34 +0100

    added deploy script

--- packages/hardhat/deploy/deploy_pop_up_store.ts
+++ packages/hardhat/deploy/deploy_pop_up_store.ts
@@ -22,10 +22,10 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  await deploy("YourContract", {
+  await deploy("USDT", {
     from: deployer,
     // Contract constructor arguments
-    args: [deployer],
+    args: [],
     log: true,
     // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
     // automatically mining the contract deployment transaction. There is no effect on live networks.
@@ -33,12 +33,24 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   });
 
   // Get the deployed contract to interact with it after deploying.
-  const yourContract = await hre.ethers.getContract<Contract>("YourContract", deployer);
-  console.log("👋 Initial greeting:", await yourContract.greeting());
+  const usdt = await hre.ethers.getContract<Contract>("USDT", deployer);
+
+  await deploy("PopUpStore", {
+    from: deployer,
+    // Contract constructor arguments
+    args: [deployer, "0x694AA1769357215DE4FAC081bf1f309aDC325306", "USDT", usdt.target],
+    log: true,
+    // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
+    // automatically mining the contract deployment transaction. There is no effect on live networks.
+    autoMine: true,
+  });
+
+  // Get the deployed contract to interact with it after deploying.
+  await hre.ethers.getContract<Contract>("PopUpStore", deployer);
 };
 
 export default deployYourContract;
 
 // Tags are useful if you have multiple deploy files and only want to run one of them.
 // e.g. yarn deploy --tags YourContract
-deployYourContract.tags = ["YourContract"];
+deployYourContract.tags = ["PopUpStore"];
